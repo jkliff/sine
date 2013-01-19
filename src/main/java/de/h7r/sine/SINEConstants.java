f05ae476b231f05d038c9a7423ae11a4cdcd2338
john <john@hesse>
2013-01-19 13:15:39 +0100

    set default listen port to localhost

--- src/main/java/de/h7r/sine/SINEConstants.java
+++ src/main/java/de/h7r/sine/SINEConstants.java
@@ -6,6 +6,6 @@ public class SINEConstants {
     public static final String META = ":meta";
 
     public static final String DEFAULT_OUTPUT_LOG_FILE = "sine.log";
-    public static final String DEFAULT_BIND_ADDRESS = "0.0.0.0";
+    public static final String DEFAULT_BIND_ADDRESS = "127.0.0.1";
     public static final String DEFAULT_PORT = "7522";
 }
